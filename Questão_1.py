642b544b5c1c2a11a64ed18616982c79f7bed1f4
PedroGustavo1 <44641023+PedroGustavo1@users.noreply.github.com>
2018-11-21 10:17:26 -0200

    Add files via upload
    Primeira Questão do Exercício sobre Pilha

---
+++ Questão_1.py
@@ -0,0 +1,48 @@
+class Pilha():
+#===================Construtor======================
+
+  def __init__(self):
+    self.pilha = []
+
+#=======Adciona Um Elemento ao Topo da Pilha========
+
+  def Push(self):
+    elemento = int(input("Digite o Elemento a Ser Adcionado: "))
+    self.pilha.append(elemento)
+    print("Sua Pilha Agora é",self.pilha)
+
+#=======Retira Um Elemento do Topo da Pilha=========
+  
+  def Pop(self):
+    if (len(self.pilha)==0):
+      print("Não Há Elementos na Sua Pilha!")
+    else:  
+      self.pilha.pop()
+      print("Sua Pilha Agora é",self.pilha)
+
+#==========Verifica se a Pilha está Vazia============
+  
+  def IsEmpty(self):
+    if len(self.pilha) == 0:
+      print("Sua Pilha Esta Vazia")
+    else:
+      print("Sua Pilha Contem Elementos")
+
+#===========Verifica o Topo da Pilha=================
+  
+  def Peek(self):
+    topo = self.pilha[-1]
+    print("O Topo da Pilha é o",topo)
+
+  #========Verifica o Tamanho da Pilha===============  
+  def Length(self):
+    tamanho = (len(self.pilha))
+    if tamanho >1:
+      print("Sua Pilha Contem %d Elementos"%(tamanho))
+    elif tamanho ==1:
+      print("Sua Pilha Contem 1 Elemento!")
+    else:
+      print("Sua Pilha Não Contem Nenhum Elemento!")
+
+p = Pilha()
+print(p.pilha)
